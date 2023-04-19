fde20db7eaf7bc43d307a824b222b68de248c034
Sheena Puthanpurayil <sheena24789@gmail.com>
2023-04-19 09:44:32 +0300

    update to API url path in env

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, Request
 import os
 import logging
-import json, xmltodict
+import json
 from kafka import KafkaProducer
 
-
 app = FastAPI()
-
 
 def get_env_variable(name):
     try:
@@ -15,8 +13,7 @@ def get_env_variable(name):
         message = "Expected environment variable '{}' not set.".format(name)
         raise Exception(message)
 
-
-# # env variables
+# env variables
 KAFKA_TOPIC = os.getenv('KAFKA_TOPIC')
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
 SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL')
@@ -24,12 +21,13 @@ SASL_MECHANISM = os.getenv('SASL_MECHANISM')
 SSL_CAFILE = os.getenv('SSL_CAFILE')
 SASL_PLAIN_USERNAME = os.getenv('SASL_PLAIN_USERNAME')
 SASL_PLAIN_PASSWORD = os.getenv('SASL_PLAIN_PASSWORD')
+LOG_LEVEL = os.getenv('LOG_LEVEL')
 
 # initialize logger
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-log = logging.getLogger(__name__)
+logging.basicConfig(level=LOG_LEVEL)
+logging.getLogger().setLevel(LOG_LEVEL)
 
+#create kafka producer
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
     security_protocol=SECURITY_PROTOCOL,
@@ -41,16 +39,31 @@ producer = KafkaProducer(
     key_serializer=lambda v: json.dumps(v).encode("ascii"),
 )
 
+@app.route("/", methods=["GET", "POST", "HEAD"])
+async def root(request: Request):
+    return {"message": "Test ok"}
 
-@app.post("/peoplecounter/v1/")
+
+# when the incoming data is xml and needs to be converted to json
+# @app.post(os.getenv('API_REQUEST_PATH'))
+# async def kafka_produce(request: Request):
+#     post_data = await request.body()
+#     logging.info(f"post observation: {post_data}")
+#     data_dict = xmltodict.parse(post_data, xml_attribs=False)
+#     producer.send(KAFKA_TOPIC,
+#                   key={"key": ""},
+#                   value={"message": data_dict}
+#                   )
+#     producer.flush()
+#     return {"message": "Kafka Produce Done"}
+
+@app.post(os.getenv('API_REQUEST_PATH'))
 async def kafka_produce(request: Request):
-    post_data = await request.body()
-    logging.debug(f"post observation: {post_data}")
-    data_dict = xmltodict.parse(post_data, xml_attribs=False)
-    print(data_dict)
+    post_data = await request.json()
+    logging.info(f"post observation: {post_data}")
     producer.send(KAFKA_TOPIC,
                   key={"key": ""},
-                  value={"message": data_dict}
+                  value={"message": post_data}
                   )
     producer.flush()
     return {"message": "Kafka Produce Done"}
